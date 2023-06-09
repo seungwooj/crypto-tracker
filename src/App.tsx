e065938811aede20ab50c77542d50885b460b1e4
AARONJ <seungwoo.aaron@gmail.com>
2023-06-09 20:04:30 +0900

    react+ts - form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import Circle from "./Circle";
+import { useState } from "react";
 
+// check ReactJS form event
 function App() {
+    const [value, setValue] = useState("");
+    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+        const {
+            currentTarget: { value },
+        } = event;
+        setValue(value);
+    };
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        console.log("event", event);
+        console.log("hello", value);
+        console.log("hello", event.target[0].value);
+    };
     return (
         <div>
-            <Circle bgColor="teal" borderColor="yellow" />
-            <Circle text="iam here" bgColor="tomato" />
+            <form onSubmit={onSubmit}>
+                <input
+                    value={value}
+                    onChange={onChange}
+                    type="text"
+                    placeholder="username"
+                />
+                <button>Log in</button>
+            </form>
         </div>
     );
 }
